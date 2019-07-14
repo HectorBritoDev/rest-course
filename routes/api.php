a0100041a95ac8f34c780f1c20770ca68213f963
HectorBritoDev <hectorbritodev@gmail.com>
2019-07-13 22:03:12 -0400

    Resource Routes

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,33 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Buyer
+ */
+Route::resource('buyers', 'Buyer\BuyerController')->only('index', 'show');
+/**
+ * Categories
+ */
+Route::resource('categories', 'Category\CategoryController')->exept('create', 'edit');
+/**
+ * Products
+ */
+Route::resource('products', 'Product\ProductController')->only('index', 'show');
+/**
+ * Sellers
+ */
+Route::resource('sellers', 'Sellers\SellersController')->only('index', 'show');
+/**
+ * Transactions
+ */
+Route::resource('transactions', 'Transaction\TransactionController')->only('index', 'show');
+/**
+ * Users
+ */
+Route::resource('users', 'User\UserController')->only('index', 'show');
