c2e9065d745d902852ab386f94c08df791fb1b54
HectorBritoDev <hectorbritodev@gmail.com>
2019-07-14 00:48:34 -0400

    Seller  relationships

--- app/Seller.php
+++ app/Seller.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use App\User;
 
-class Seller extends Model
+class Seller extends User
 {
-    //
+    public function products()
+    {
+        return $this->hasMany('App\Product');
+    }
 }
