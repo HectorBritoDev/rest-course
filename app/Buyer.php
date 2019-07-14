80f8ed93d0f62c0cee4e039e280f47ff5dd47186
HectorBritoDev <hectorbritodev@gmail.com>
2019-07-14 00:48:13 -0400

    Buyer relationships

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use App\User;
 
-class Buyer extends Model
+class Buyer extends User
 {
-    //
+    public function transactions()
+    {
+        return $this->hasMany('App\Transaction');
+    }
 }
