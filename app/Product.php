7631aaedeba61a7db78e4933b79653ed14fb6332
HectorBritoDev <hectorbritodev@gmail.com>
2019-07-14 00:48:30 -0400

    Product relationships

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function estaDiponible()
+    {
+        return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category');
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany('App\Transaction');
+    }
 }
