5805fe9c07d47f945a4cd8835f2a379f9f3f223e
HectorBritoDev <hectorbritodev@gmail.com>
2019-07-19 01:13:06 -0400

    seeder fixed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
         Transaction::truncate();
         DB::table('category_product')->truncate();
 
-        $cantidadUsuarios = 200;
+        $cantidadUsuarios = 1000;
         $cantidadCategorias = 30;
         $cantidadProductos = 1000;
         $cantidadTransacciones = 1000;
@@ -38,6 +38,6 @@ class DatabaseSeeder extends Seeder
             }
         );
 
-        factory(Transaction::class, $cantidadUsuarios)->create();
+        factory(Transaction::class, $cantidadTransacciones)->create();
     }
 }
