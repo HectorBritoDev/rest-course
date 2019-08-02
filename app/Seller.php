3ac40cc9c97690ebc4ab5d195826e13752dab760
HectorBritoDev <hectorbritodev@gmail.com>
2019-08-02 17:53:09 -0400

    Global scopes added to Seller

--- app/Seller.php
+++ app/Seller.php
@@ -2,10 +2,20 @@
 
 namespace App;
 
+use App\Scopes\SellerScope;
 use App\User;
 
 class Seller extends User
 {
+    protected static function boot()
+    {
+        //Boot del modelo base o padre, necesario para el funcionamiento original de Laravel
+        parent::boot();
+
+        //Se utiliza "static" porque estamos en un metodo estatico y queremos hacer referencia a un metodo propio de la clase
+        static::addGlobalScope(new SellerScope);
+    }
+
     public function products()
     {
         return $this->hasMany('App\Product');
