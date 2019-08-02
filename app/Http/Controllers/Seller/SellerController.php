de8fffbc8707d56de942d485a1a9aa578d63852e
HectorBritoDev <hectorbritodev@gmail.com>
2019-08-02 17:21:54 -0400

    added Apicontroller to ControllerController

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -2,22 +2,22 @@
 
 namespace App\Http\Controllers\Seller;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use App\Seller;
 
-class SellerController extends Controller
+class SellerController extends ApiController
 {
 
     public function index()
     {
         $vendedores = Seller::has('products')->get();
-        return response()->json(['data' => $vendedores], 200);
+        return $this->showAll($vendedores);
 
     }
     public function show($id)
     {
         $vendedor = Seller::has('products')->findOrFail($id);
-        return response()->json(['data' => $vendedor], 200);
+        return $this->showOne($vendedor);
     }
 
 }
