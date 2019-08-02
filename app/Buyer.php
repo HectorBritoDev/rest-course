0f6c5dd83e8ffcaa629721657bc15bdf86cd1f8d
HectorBritoDev <hectorbritodev@gmail.com>
2019-08-02 17:53:05 -0400

    Global scopes added to Buyer

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,10 +2,19 @@
 
 namespace App;
 
+use App\Scopes\BuyerScope;
 use App\User;
 
 class Buyer extends User
 {
+    protected static function boot()
+    {
+        //Boot del modelo base o padre, necesario para el funcionamiento original de Laravel
+        parent::boot();
+
+        //Se utiliza "static" porque estamos en un metodo estatico y queremos hacer referencia a un metodo propio de la clase
+        static::addGlobalScope(new BuyerScope);
+    }
     public function transactions()
     {
         return $this->hasMany('App\Transaction');
