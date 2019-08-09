9551303c53a8ef505e981ec96d902fb50e9c4a8b
HectorBritoDev <hectorbritodev@gmail.com>
2019-08-09 19:38:58 -0400

    Markdown added to mail views

---
+++ resources/views/mails/confirm.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Hola {{ $user->name }}
+
+Has cambiado tu correo electrónico. Por favor verifica la nueva dirección usando el siguiente boton:
+
+@component('mail::button', ['url' => route('verify',$user->verification_token) ])
+Confirmar tu cuenta
+@endcomponent
+
+Gracias,<br>
+{{ config('app.name') }}
+@endcomponent
