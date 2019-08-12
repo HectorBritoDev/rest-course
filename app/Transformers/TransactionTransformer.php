f8b645ca0ff0fe9123a056007f05307350fc8bdf
HectorBritoDev <hectorbritodev@gmail.com>
2019-08-12 16:30:16 -0400

    TransformedAttributes added to TransactionTransformer

--- app/Transformers/TransactionTransformer.php
+++ app/Transformers/TransactionTransformer.php
@@ -62,5 +62,19 @@ class TransactionTransformer extends TransformerAbstract
 
         return isset($attributes[$index]) ? $attributes[$index] : null;
     }
+    public static function transformedAttributes($index)
+    {
+        $attributes = [
+            'id' => 'identificador',
+            'quantity' => 'cantidad',
+            'buyer_id' => 'comprador',
+            'product_id' => 'producto',
+            'created_at' => 'fechaCreacion',
+            'updated_at' => 'fechaActualizacion',
+            'deleted_at' => 'fechaEliminacion',
+        ];
+
+        return isset($attributes[$index]) ? $attributes[$index] : null;
+    }
 
 }
